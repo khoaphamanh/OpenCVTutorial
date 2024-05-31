40150df81c0ed3c44a2f1bdf0c1a722ff3685b6f
khoaphamanh <pham.anhkhoa1215@gmail.com>
2024-05-31 18:42:17 +0200

    webcam full screen

--- 1_basic/2_open_webcam.py
+++ 1_basic/2_open_webcam.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 # open webcam
 cap = cv2.VideoCapture(0)
 
+# window name Ergebnis in full screen mode
+window_name = "Your webcam"
+cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
 while True:
     # read the webcam
     ret, frame = cap.read()
@@ -14,7 +19,7 @@ while True:
     print("frame:", frame.shape)
 
     # webcam as a window
-    cv2.imshow("Your webcam", frame)
+    cv2.imshow(window_name, frame)
 
     # cv2.waitKey(1) for 1 milisecond for a key event, if q pressed, break the loop
     if cv2.waitKey(1) & 0xFF == ord("q"):
